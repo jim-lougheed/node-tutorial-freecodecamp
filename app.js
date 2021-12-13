0800f2982e6132f51e954106e61c87d87d50c982
Jim Lougheed <jims.codes@gmail.com>
2021-12-13 16:25:34 -0500

    Complete module 7

--- app.js
+++ app.js
@@ -3,9 +3,10 @@
 const { john, peter } = require('./4-names');
 const sayHi = require('./5-utils');
 const { items, singlePerson } = require('./6-alternative-flavour');
+require('./7-mind-grenade')
 console.log(singlePerson)
 console.log(items)
 
-sayHi('susan');
-sayHi(john);
-sayHi(peter);
+// sayHi('susan');
+// sayHi(john);
+// sayHi(peter);
