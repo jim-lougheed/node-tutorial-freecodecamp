4ddbe4e31f79ab664cd1761a5fffa4b71689795b
Jim Lougheed <jims.codes@gmail.com>
2021-12-13 15:56:28 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//CommonJS, every file is module (by default)
+//Modules - encapsulated code (only share minimum)
+const { john, peter } = require('./4-names');
+const sayHi = require('./5-utils');
+const { items, singlePerson } = require('./6-alternative-flavour');
+console.log(singlePerson)
+console.log(items)
+
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
