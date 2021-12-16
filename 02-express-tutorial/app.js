88cf3af45d76687a0d8576ccbcea1b4f6e28696b
Jim Lougheed <jims.codes@gmail.com>
2021-12-16 17:15:42 -0500

    Complete Express AllStatic (5:18:16)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    console.log('user hit the home route')
-    res.status(200).send('Home Page')
-})
+// set up static and middleware
+app.use(express.static('./public'));
 
-app.get('about', (req, res) => {
-    res.status(200).send('About Page')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
-app.all('*', (req, res) => {
-    res.status(404).send('Page Not Found')
-})
+app.all('*'), (req, res) => {
+    res.status(404).send('Page not found')
+}
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT)
+    console.log(`Server is listening on port ${PORT}`)
 })
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
