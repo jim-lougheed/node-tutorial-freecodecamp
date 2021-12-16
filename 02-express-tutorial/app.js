446e30778e692e43eb9aacc23e38875e73d8301a
Jim Lougheed <jims.codes@gmail.com>
2021-12-16 17:38:43 -0500

    Complete JSON basics (5:32:43)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-
 const PORT = 3000;
+const { products } = require('./data.js');
 
-// set up static and middleware
-app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
-app.all('*'), (req, res) => {
-    res.status(404).send('Page not found')
-}
+app.get('/', (req, res) => {
+    res.json(products)
+})
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+    console.log(`Server is listening on port ${PORT}`);
 })
