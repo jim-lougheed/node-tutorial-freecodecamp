4b5f39fafc936b2b4e4d4b3933e6ef798a6ca268
Jim Lougheed <jims.codes@gmail.com>
2021-12-17 15:32:07 -0500

    Complete Route Params (5:50:48)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,7 +4,26 @@ const PORT = 3000;
 const { products } = require('./data.js');
 
 app.get('/', (req, res) => {
-    res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productID', (req, res) => {
+    const singleProduct = products.find((product) => {
+        return product.id === Number(req.params.productID)
+    })
+    if (!singleProduct) {
+        res.status(404).send('Product not found')
+    } else {
+        res.status(200).json(singleProduct);
+    }
 })
 
 app.listen(PORT, () => {
