8e5f6d6574b18e442933a7982541fc76c6ab3664
Jim Lougheed <jims.codes@gmail.com>
2021-12-17 16:17:23 -0500

    Complete APP.USE (6:28:36)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,57 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const { products } = require('./data.js');
+const logger = require('./logger');
+
+// req => middleware => res
+app.use('/api', logger);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+    res.send('Home');
+})
+
+app.get('/about', (req, res) => {
+    res.send('About');
 })
 
 app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image}
-    })
-    res.json(newProducts)
+    res.send('Products');
 })
 
-app.get('/api/products/:productID', (req, res) => {
-    const singleProduct = products.find((product) => {
-        return product.id === Number(req.params.productID)
-    })
-    if (!singleProduct) {
-        res.status(404).send('Product not found')
-    } else {
-        res.status(200).json(singleProduct);
-    }
-})
-
-app.get('/api/products/:productID/reviews/reviewID', (req, res) => {
-    console.log(req.params);
-    res.send('Hello World')
-})
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query);
-    const { search, limit } = req.query;
-    let sortedProducts = [...products]
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if (sortedProducts.length < 1) {
-        // return res.status(200).send('No product matches')
-        return res.status(200).json({ success: true })
-    }
-    res.status(200).json(sortedProducts);
-    // res.send('Hello World')
+app.get('/api/items', (req, res) => {
+    res.send('Items');
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
