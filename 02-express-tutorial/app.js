8fa02b3c32037fa5fe21f664b0e5e37acdd9ab3a
Jim Lougheed <jims.codes@gmail.com>
2021-12-18 22:38:28 -0500

    Complete PUT Method (7:41:55)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -41,6 +41,24 @@ app.post('/login', (req, res) => {
     }
 })
 
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const person = people.find((person) => person.id === Number(id))
+
+    if (!person) {
+        return res.status(404).json({ success: false, msg: 'Please provide a name'})
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({ success: true, data: newPeople })
+    
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
