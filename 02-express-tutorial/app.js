5eee315038f192a537228bf95319a1e7f42edb65
Jim Lougheed <jims.codes@gmail.com>
2021-12-18 22:50:12 -0500

    Complete DELETE Method (7:50:18)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -59,6 +59,16 @@ app.put('/api/people/:id', (req, res) => {
     
 })
 
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+
+    if (!person) {
+        return res.status(404).json({ success: false, msg: `No person with id ${req.params.id}`})
+    }
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+    return res.status(200).json({ success: true, data: newPeople })
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
