575c7131922f0dd6f4ba2b5278026366491e6e75
Jim Lougheed <jims.codes@gmail.com>
2021-12-18 22:25:41 -0500

    Complete Postman (7:30:41)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -24,6 +24,14 @@ app.post('/api/people', (req, res) => {
     res.status(201).send({ success: true, person: name })
 })
 
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, msg: 'Please provide a name'})
+    }
+    res.status(201).send({ success: true, data: [...people, name] })
+})
+
 app.post('/login', (req, res) => {
     const { name } = req.body;
     if (name) {
